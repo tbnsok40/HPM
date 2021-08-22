1262332858b399a92376e1cfb6cefbe83292bfb9
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-08-22 21:59:25 +0900

    [add] resultpage css 작업

--- hospital-front/src/components/ResultPage.tsx
+++ hospital-front/src/components/ResultPage.tsx
@@ -1,11 +1,48 @@
-
+import poster from "../슬의.png";
+import {Link} from "react-router-dom";
+import "../style/Result.css"
 
 const ResultPage = () => {
 
 
-    return(
-        <div>123</div>
-    )
+    return (
+        <section id="main_contents">
+            <div className="wrapper">
+                <div className="upper-result">
+                    <div className="result_title_container" style={{
+                        "position": "relative"
+                    }}>
+                        나와 닮은 의사는 <br/>
+                        OOO입니다.
+                        <div className="result_title_above"
+                             style={{"position": "absolute", "top": "-45px", "right": "1px"}}>
+                            ++
+                        </div>
+                    </div>
+                    <div className="image_container" style={{
+                        "position": "relative",
+                        "top": "-20px",
+                    }}>
+                        <img id="poster" src={poster} alt="슬의"/>
+                    </div>
+                </div>
+                <div className="bottom-result">
+                    <button className="result-button" type="button">테스트 다시하기</button>
+                    <div id="sns-buttons">
+                        <button className="sns-button" type="button">
+                            카카오
+                        </button>
+                        <button className="sns-button" type="button">
+                            인스타
+                        </button>
+                        <button className="sns-button" type="button">
+                            페이스북
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </section>)
 }
 
 export default ResultPage;
