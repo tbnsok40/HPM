4ad1a6c9f119e5a121e600f737b0094b5df5139e
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-08-25 15:41:04 +0900

    [add] backend 로직 정리중

--- hospital-back/src/hospital/hospital.service.ts
+++ hospital-back/src/hospital/hospital.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Questions } from "../entities/Questions";
 import { Repository } from "typeorm";
 import { MBTI } from "./constants";
+import { switchType } from "./utils";
 
 @Injectable()
 export class HospitalService {
@@ -16,8 +17,18 @@ export class HospitalService {
     return this.questionRepository.find();
   }
 
+  async getResult(mbtiType) {
+    const resultId:number = switchType(mbtiType);
+    // return await this.newRepository.findOne(resultId); // typeorm 은 getbyId (x)
+  }
+
   // 응답결과 배열 기반으로 유형 연산 로직
   saveAnswers(resultArray) {
+
+    let mbti = {}
+    resultArray.forEach(result => {
+      mbti[result] = (mbti[result] || 0) + 1
+    })
 
     const countElements = (array, x) => array.filter(arr => x === arr).length
     const data = new Set(resultArray.filter(result => countElements(resultArray, result) > 1))
@@ -27,7 +38,14 @@ export class HospitalService {
         finalResult += type
       }
     })
-    console.log(finalResult); // 개별 유형인 resultArray 있으면 된다. 저장할 것
+    console.log(finalResult); // 개별 유형인 resultArray 있으면 된다. 저장할 것.
+    this.getResult(finalResult) // MBTI type
     // return this.questionRepository.save();
   }
 }
+
+// finalResult 가 나왔으니 이제는 switch 문을 사용할 때 =>
+// switch()의 결과에 따라 getById() 로 결과테이블을 조회. => 결과 테이블 스키마 어떻게 구성할 것인지.
+// get id 어노테이션 추가하여 function 및 로직 생성
+// return 받은 데이터로 결과지에 뿌린다.
+
